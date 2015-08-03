db531fdc073ecf8c23e9833bb49b719a6b80ff1b
moru <kevin.singer@student.tuwien.ac.at>
2015-08-04 01:31:01 +0200

    Todo textfield.

--- app/app.js
+++ app/app.js
@@ -3,3 +3,23 @@ console.log('hello world!');
 import Rx from 'rx'
 
 window.Rx = Rx; //for debugging
+
+var btn = document.getElementById('masterbutton');
+
+var clicks = Rx.Observable.fromEvent(btn, 'click')
+             .map(() => 1)
+             .scan((a,b) => a+b)
+             .subscribe((e) => console.log('clicked ' + e + '-times'));
+
+
+var todoBtn = document.getElementById('todo-button');
+var todoText = document.getElementById('todo-text');
+window.todoText = todoText;
+
+var addButtonClicks = Rx.Observable.fromEvent(todoBtn, 'click');
+
+var todoItemsAdded  = addButtonClicks.map(() => todoText.value);
+
+// http://cycle.js.org/model-view-intent.html
+// https://github.com/Reactive-Extensions/RxJS/blob/master/doc/api/core/observable.md
+// .reduce(...)
