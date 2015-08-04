6e55893e8a3da50cd92fc77208ac0c9d531988d6
moru <kevin.singer@student.tuwien.ac.at>
2015-08-05 00:41:23 +0200

    Issues with corejs import.

--- app/app.js
+++ app/app.js
@@ -1,24 +1,97 @@
-console.log('hello world!');
+import Rx from 'rx';
 
-import Rx from 'rx'
+import Cycle from '@cycle/core';
+//import CycleDOM from '@cycle/dom';
+import CycleDOM from '@cycle/dom';
+let {h} = CycleDOM;
+
+console.log('app.js loaded.');
+console.log(h);
 
 window.Rx = Rx; //for debugging
+window.Cycle = Cycle; //for debugging
 
-var btn = document.getElementById('masterbutton');
-
-var clicks = Rx.Observable.fromEvent(btn, 'click')
+let btn = document.getElementById('masterbutton');
+let clicks = Rx.Observable.fromEvent(btn, 'click')
              .map(() => 1)
              .scan((a,b) => a+b)
              .subscribe((e) => console.log('clicked ' + e + '-times'));
 
 
-var todoBtn = document.getElementById('todo-button');
-var todoText = document.getElementById('todo-text');
-window.todoText = todoText;
+function cyclejsMain(drivers) {
+    return {
+        DOM: drivers.DOM.get('.myinput', 'input')
+            .map(ev => ev.target.value)
+            .startsWith('')
+            .map(name =>
+                h('div')
+                /*h('div', [
+                    h('label', 'Name:'),
+                    h('input.myinput', {
+                        attributes: {type: 'text'}
+                    }),
+                    h('p', 'Hello ' + name)
+                ])*/
+            )
+    };
+}
+let cycleDrivers = {
+    DOM: CycleDOM.makeDOMDriver('#helloCycleContainer')
+}
 
-var addButtonClicks = Rx.Observable.fromEvent(todoBtn, 'click');
+Cycle.run(cyclejsMain, cycleDrivers);
 
-var todoItemsAdded  = addButtonClicks.map(() => todoText.value);
+
+
+
+let todoBtn = document.getElementById('todo-button');
+let todoText = document.getElementById('todo-text');
+let todoList = document.getElementById('todo-list');
+
+let addButtonClicks = Rx.Observable.fromEvent(todoBtn, 'click');
+
+let actionStream  = addButtonClicks.map(() => ({add: todoText.value}));
+
+let currentItems = actionStream.scan(function(items, action){
+    let newItems;
+    let toAdd = [];
+    if(action.add) { toAdd = action.add }
+    newItems = items.concat(toAdd); //clones the array as side-effect
+    console.log('actionStream.scan:action: ', action)
+    if(action.remove) {
+        console.log('newItems before: ', newItems);
+        newItems.splice(action.remove + 1, 1);
+        console.log('newItems after: ', newItems);
+    }
+    return newItems
+}, []);
+
+currentItems.subscribe(function (items) {
+    todoList.innerHTML = '';
+    let closeButtonsStream = Rx.Observable.empty();
+    for(let i = items.length - 1; i >= 0; i--) {
+        let itm = items[i];
+
+        let li = document.createElement('li');
+        li.innerHTML = itm + ' ';
+
+        let a = document.createElement('a');
+        a.href = '#';
+        a.innerHTML = 'x';
+
+        li.appendChild(a);
+        todoList.appendChild(li);
+
+        let individualCloseBtnStream = Rx.Observable.fromEvent(a, 'click').map(() => ({remove: i}));
+        closeButtonsStream = closeButtonsStream.merge(individualCloseBtnStream);
+    }
+    actionStream = actionStream.merge(closeButtonsStream);
+    // TODO not the same actionStream-object anymore
+    // all listeners stay with the old one
+    actionStream.subscribe((idx) => console.log('action: ', idx))
+});
+
+currentItems.subscribe(items => console.log(items));
 
 // http://cycle.js.org/model-view-intent.html
 // https://github.com/Reactive-Extensions/RxJS/blob/master/doc/api/core/observable.md
--- jspm.config.js
+++ jspm.config.js
@@ -1,5 +1,6 @@
 System.config({
   "baseURL": "/",
+  "defaultJSExtensions": true,
   "transpiler": "babel",
   "babelOptions": {
     "optional": [
@@ -7,21 +8,47 @@ System.config({
     ]
   },
   "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
   }
 });
 
 System.config({
   "map": {
+    "@cycle/core": "npm:@cycle/core@1.0.2",
+    "@cycle/dom": "npm:@cycle/dom@3.2.1",
     "babel": "npm:babel-core@5.7.4",
     "babel-runtime": "npm:babel-runtime@5.7.0",
     "core-js": "npm:core-js@0.9.18",
-    "rx": "npm:rx@2.5.3",
+    "cyclejs": "npm:cyclejs@0.24.1",
+    "rx": "npm:rx@3.0.0",
     "system-image": "npm:system-image@0.1.0",
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-events@0.1.1": {
+      "events": "npm:events@1.0.2"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:@cycle/core@1.0.2": {
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "rx": "npm:rx@2.5.3"
+    },
+    "npm:@cycle/dom@3.2.1": {
+      "@cycle/core": "npm:@cycle/core@1.0.2",
+      "es6-map": "npm:es6-map@0.1.1",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "vdom-parser": "npm:vdom-parser@1.2.1",
+      "vdom-to-html": "npm:vdom-to-html@2.1.1",
+      "virtual-dom": "npm:virtual-dom@2.1.0"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.7.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
@@ -31,8 +58,116 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
+    "npm:cyclejs@0.24.1": {
+      "es6-map": "npm:es6-map@0.1.1",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "rx": "npm:rx@2.5.3",
+      "vdom-to-html": "npm:vdom-to-html@2.1.1",
+      "virtual-dom": "npm:virtual-dom@2.0.1"
+    },
+    "npm:d@0.1.1": {
+      "es5-ext": "npm:es5-ext@0.10.7"
+    },
+    "npm:error@4.4.0": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "camelize": "npm:camelize@1.0.0",
+      "string-template": "npm:string-template@0.2.1",
+      "xtend": "npm:xtend@4.0.0"
+    },
+    "npm:es5-ext@0.10.7": {
+      "es6-iterator": "npm:es6-iterator@0.1.3",
+      "es6-symbol": "npm:es6-symbol@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:es6-iterator@0.1.3": {
+      "d": "npm:d@0.1.1",
+      "es5-ext": "npm:es5-ext@0.10.7",
+      "es6-symbol": "npm:es6-symbol@2.0.1"
+    },
+    "npm:es6-map@0.1.1": {
+      "d": "npm:d@0.1.1",
+      "es5-ext": "npm:es5-ext@0.10.7",
+      "es6-iterator": "npm:es6-iterator@0.1.3",
+      "es6-set": "npm:es6-set@0.1.1",
+      "es6-symbol": "npm:es6-symbol@0.1.1",
+      "event-emitter": "npm:event-emitter@0.3.3"
+    },
+    "npm:es6-set@0.1.1": {
+      "d": "npm:d@0.1.1",
+      "es5-ext": "npm:es5-ext@0.10.7",
+      "es6-iterator": "npm:es6-iterator@0.1.3",
+      "es6-symbol": "npm:es6-symbol@0.1.1",
+      "event-emitter": "npm:event-emitter@0.3.3"
+    },
+    "npm:es6-symbol@0.1.1": {
+      "d": "npm:d@0.1.1",
+      "es5-ext": "npm:es5-ext@0.10.7"
+    },
+    "npm:es6-symbol@2.0.1": {
+      "d": "npm:d@0.1.1",
+      "es5-ext": "npm:es5-ext@0.10.7"
+    },
+    "npm:ev-store@7.0.0": {
+      "individual": "npm:individual@3.0.0"
+    },
+    "npm:event-emitter@0.3.3": {
+      "d": "npm:d@0.1.1",
+      "es5-ext": "npm:es5-ext@0.10.7",
+      "events": "github:jspm/nodelibs-events@0.1.1"
+    },
+    "npm:global@4.3.0": {
+      "process": "npm:process@0.5.2"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:next-tick@0.2.2": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:param-case@1.1.1": {
+      "sentence-case": "npm:sentence-case@1.1.2"
+    },
     "npm:rx@2.5.3": {
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:rx@3.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:sentence-case@1.1.2": {
+      "lower-case": "npm:lower-case@1.1.2"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:vdom-parser@1.2.1": {
+      "virtual-dom": "npm:virtual-dom@2.1.0"
+    },
+    "npm:vdom-to-html@2.1.1": {
+      "escape-html": "npm:escape-html@1.0.2",
+      "param-case": "npm:param-case@1.1.1",
+      "virtual-dom": "npm:virtual-dom@2.1.0",
+      "xtend": "npm:xtend@4.0.0"
+    },
+    "npm:virtual-dom@2.0.1": {
+      "browser-split": "npm:browser-split@0.0.1",
+      "error": "npm:error@4.4.0",
+      "ev-store": "npm:ev-store@7.0.0",
+      "global": "npm:global@4.3.0",
+      "is-object": "npm:is-object@1.0.1",
+      "next-tick": "npm:next-tick@0.2.2",
+      "x-is-array": "npm:x-is-array@0.1.0",
+      "x-is-string": "npm:x-is-string@0.1.0"
+    },
+    "npm:virtual-dom@2.1.0": {
+      "browser-split": "npm:browser-split@0.0.1",
+      "error": "npm:error@4.4.0",
+      "ev-store": "npm:ev-store@7.0.0",
+      "global": "npm:global@4.3.0",
+      "is-object": "npm:is-object@1.0.1",
+      "next-tick": "npm:next-tick@0.2.2",
+      "x-is-array": "npm:x-is-array@0.1.0",
+      "x-is-string": "npm:x-is-string@0.1.0"
     }
   }
 });
